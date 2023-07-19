6894d7dd372b7da84a49e24e6c0ec9f13b1ea66c
Shajahan A <Shajansheriff@users.noreply.github.com>
2023-07-19 18:34:39 +0530

    change font to montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`${inter.className} h-full w-full`}>{children}</body>
+      <body className={`${font.className} h-full w-full`}>{children}</body>
     </html>
   );
 }
